6274a17f5c91512ea00ff17fbcd8ccd9c8eead16
deprak <dedprakasa@gmail.com>
2020-01-16 19:09:39 +0700

    speech to text server done

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const port = process.env.PORT
+const routes = require('./routes')
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+app.use('/', routes)
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
